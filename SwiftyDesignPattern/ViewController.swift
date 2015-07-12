5dc805ad09f3521b791168064a5c6ff0dba30efe
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2015-07-12 23:07:30 +0900

    Initial Commit

---
+++ SwiftyDesignPattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyDesignPattern
+//
+//  Created by 横山 拓也 on 2015/07/12.
+//  Copyright (c) 2015年 Takuya Yokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
